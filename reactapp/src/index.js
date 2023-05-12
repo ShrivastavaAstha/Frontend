628febf93dff9d73a234c65c99dd89cf467a05d7
Astha <astha2333@gmail.com>
2023-05-12 22:36:20 +0530

    Fontend

---
+++ reactapp/src/index.js
@@ -0,0 +1,5 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+//ReactDOM.render("kya dikhana hai", "kaha dikhana hai", "cllback function");
+ReactDOM.render(<h1>Hellow World!!</h1>, document.getElementById("root"));
