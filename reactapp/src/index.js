caa84a9f824be491b2168d4a27291c49c7b89b63
Astha <astha2333@gmail.com>
2023-05-12 23:52:39 +0530

    Frontend

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -2,4 +2,17 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 
 //ReactDOM.render("kya dikhana hai", "kaha dikhana hai", "cllback function");
+
+//First way:
 ReactDOM.render(<h1>Hellow World!!</h1>, document.getElementById("root"));
+
+//Second way:
+// ReactDOM.render(
+//   React.createElement("h1", null, "Astha"),
+//   document.getElementById("root")
+// );
+
+//Third way:
+var h1 = document.createElement("h1");
+h1.innerHTML = "ASTHA";
+document.getElementById("root").appendChild(h1);
