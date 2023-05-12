58bac804e324aed5e6c4be3f89b8c26f00f187dc
Astha <astha2333@gmail.com>
2023-05-13 00:16:34 +0530

    update

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,10 +1,32 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+const arr = ["astha", "khushi", "roshni"];
+//In React it's possible for render() to return an array of elements.
 //ReactDOM.render("kya dikhana hai", "kaha dikhana hai", "cllback function");
 
 //First way:
-ReactDOM.render(<h1>Hellow World!!</h1>, document.getElementById("root"));
+
+//------------>>>>Array Method
+ReactDOM.render(
+  [
+    <h1>Hello World!!</h1>,
+    <p>Welcome to this new World!</p>,
+    <h2>Glad to see you all.</h2>,
+  ],
+  document.getElementById("root")
+);
+
+//------------->>Div Method
+ReactDOM.render(
+  <div>
+    <h1>Hello World!!</h1>
+    <p>Welcome to this new World!</p>
+    <h2>Glad to see you all.</h2>
+  </div>,
+
+  document.getElementById("root")
+);
 
 //Second way:
 // ReactDOM.render(
@@ -13,6 +35,6 @@ ReactDOM.render(<h1>Hellow World!!</h1>, document.getElementById("root"));
 // );
 
 //Third way:
-var h1 = document.createElement("h1");
-h1.innerHTML = "ASTHA";
-document.getElementById("root").appendChild(h1);
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "ASTHA";
+// document.getElementById("root").appendChild(h1);
