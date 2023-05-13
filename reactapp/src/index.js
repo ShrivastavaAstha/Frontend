5c4421688a68b226889bbf660c826a17e104dcc1
Astha <astha2333@gmail.com>
2023-05-13 09:29:27 +0530

    Frontend

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -28,6 +28,27 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+//------------>>React Fragment
+ReactDOM.render(
+  <React.Fragment>
+    <h1>Hello World!!</h1>
+    <p>Welcome to this new World!</p>
+    <h2>Glad to see you all.</h2>
+  </React.Fragment>,
+
+  document.getElementById("root")
+);
+
+//------------->>Syntactical sugar for fragments
+ReactDOM.render(
+  <>
+    <h1>Hello World!!</h1>
+    <p>Welcome to this new World!</p>
+    <h2>Glad to see you all.</h2>
+  </>,
+
+  document.getElementById("root")
+);
 //Second way:
 // ReactDOM.render(
 //   React.createElement("h1", null, "Astha"),
