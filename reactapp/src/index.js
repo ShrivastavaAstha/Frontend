f1491501ae6a5289bf417cdcfc915b5a22f006ec
Astha <astha2333@gmail.com>
2023-05-13 11:12:14 +0530

    Frontend

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -39,7 +39,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-//------------->>Syntactical sugar for fragments
+//----------->>Syntactical sugar for fragments
 ReactDOM.render(
   <>
     <h1>Hello World!!</h1>
@@ -50,12 +50,73 @@ ReactDOM.render(
   document.getElementById("root")
 );
 //Second way:
-// ReactDOM.render(
-//   React.createElement("h1", null, "Astha"),
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  React.createElement("h1", null, "Astha"),
+  document.getElementById("root")
+);
 
 //Third way:
-// var h1 = document.createElement("h1");
-// h1.innerHTML = "ASTHA";
-// document.getElementById("root").appendChild(h1);
+//var h1 = document.createElement("h1");
+//h1.innerHTML = "ASTHA";
+//document.getElementById("root").appendChild(h1);
+
+//-------------------------------Netflix Program---------------------
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>Netflix Pick</h1>
+    <p>Here the list of 5 netflix series</p>
+    <ol>
+      <li>Dark</li>
+      <li>Extra curricular</li>
+      <li>My Holo Love</li>
+      <li>My first-2 love</li>
+      <li>Mr Robot</li>
+    </ol>
+  </>,
+  document.getElementById("root")
+);
+//---------------------------------------------------------------------
+
+//------------------ Expressions in JSX -------------------------------
+const flname = "Astha Srivastava";
+ReactDOM.render(
+  <>
+    <h1>My name is {flname} </h1>
+    <p>My lucky number is {5 + 6}</p>
+  </>,
+  document.getElementById("root")
+);
+//----------------------------------------------------------------------
+
+//--------------------------Concatination-------------------------------
+const fname = "Astha";
+const lname = "Srivastava";
+
+//-------First way through curly braces.
+ReactDOM.render(
+  <>
+    <h1>
+      My name is {fname} {lname}
+    </h1>
+  </>,
+  document.getElementById("root")
+);
+//-------Second way through concatination.
+ReactDOM.render(
+  <>
+    <h1>My name is {fname + " " + lname}</h1>
+  </>,
+  document.getElementById("root")
+);
+//--------Third way through Template Literals.
+ReactDOM.render(
+  <>
+    <h1>{`My name is ${fname} ${lname}`}</h1>
+    <h2>{`My first name is ${fname} and my last name is ${lname}.`}</h2>
+  </>,
+  document.getElementById("root")
+);
+//------------------------------------------------------------------------
