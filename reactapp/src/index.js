24be56b5d399d28a2af569fe4163f081fba70fbf
Astha <astha2333@gmail.com>
2023-05-13 11:43:03 +0530

    Frontend

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -120,3 +120,20 @@ ReactDOM.render(
   document.getElementById("root")
 );
 //------------------------------------------------------------------------
+
+//--------------------------------------Practice--------------------------
+// import React from "react";
+// import ReactDOM from "react-dom";
+const name = "Astha";
+//const currDate = new Date().getDate();
+const currDate = new Date().toLocaleDateString();
+const currTime = new Date().toLocaleTimeString();
+ReactDOM.render(
+  <>
+    <h1>Hello, My name is {name}</h1>
+    <p>Current Date is = {currDate} </p>
+    <p>Current Time is = {currTime} </p>
+  </>,
+  document.getElementById("root")
+);
+//--------------------------------------------------------------------------
