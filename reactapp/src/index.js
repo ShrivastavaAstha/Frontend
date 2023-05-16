352a1b4720586873f39a3c09531c6ab689a15db8
Astha <astha2333@gmail.com>
2023-05-16 22:35:45 +0530

    Frontend

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -183,3 +183,28 @@ ReactDOM.render(
 //---------------------------------------------------------------------------
 
 //-------------------------------
+//import React from "react";
+//import ReactDOM from "react-dom";
+
+let curDate = new Date(2023, 5, 16, 5);
+curDate = curDate.getHours();
+let greeting = "";
+const cssStyle = {};
+if (curDate >= 1 && curDate < 12) {
+  greeting = "Good Morning";
+  cssStyle.color = "blue";
+} else if (curDate >= 12 && curDate < 19) {
+  greeting = "Good Afternoon";
+  cssStyle.color = "orange";
+} else {
+  greeting = "Good Night";
+  cssStyle.color = "Blue";
+}
+ReactDOM.render(
+  <>
+  <div>
+  <h1>
+    Hello Sir,<span style={cssStyle}> {greeting}</span>
+  </h1>,
+  document.getElementById("root")
+);
